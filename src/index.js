2cd92a1d984b5ca5e2ac91632814299cb066c69b
olegsey <71653455+Oleg11713@users.noreply.github.com>
2021-02-28 16:02:20 +0300

    fix: change conditions

--- src/index.js
+++ src/index.js
@@ -1,28 +1,37 @@
 
 exports.min = function min (array) {
-  let min = array[0];
+  if (Array.isArray(array) && array.length != 0){
+    let min = array[0];
     for (let i = 1; i < array.length; i++){
         if(array[i] < min)
         min = array[i];
     }
     return min;
+    }
+    else return 0;
 }
 
 exports.max = function max (array) {
-  let max = array[0];
+   if (Array.isArray(array) && array.length != 0){
+    let max = array[0];
     for (let i = 1; i < array.length; i++){
         if(array[i] > max)
         max = array[i];
     }
     return max;
+    }
+    else return 0;
 }
 
 exports.avg = function avg (array) {
-  let sum = 0;
+  if (Array.isArray(array) && array.length != 0){
+    let sum = 0;
     let count = 0;
     for (let i = 0; i < array.length; i++){
         sum += array[i];
         count++;
     }
     return sum/count;
+    }
+    else return 0;
 }
